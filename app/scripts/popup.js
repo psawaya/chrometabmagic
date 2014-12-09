9331361acd1c2ec509f2c1cac55203e62ee8fdf8
Paul Sawaya <me@paulsawaya.com>
2014-12-09 14:18:33 -0800

    lists tabs in browser action

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -3,21 +3,17 @@
 // To appease jshint.
 var React = window.React;
 
-var testData = {
-  items: [
-    {
-      title: 'Foo'
-    },
-    {
-      title: 'Bar'
-    }
-  ]
-};
-
 var SearchResultItem = React.createClass({
   displayName: 'SearchResultItem',
   render: function() {
-    return React.createElement('div', {}, 'I\'m an item!');
+    return React.createElement('div', {}, [
+      React.createElement('img', {
+        src: this.props.favIconUrl,
+        width: 16,
+        height: 16
+      }),
+      React.createElement('span', {}, [this.props.title])
+    ]);
   }
 });
 
@@ -25,7 +21,10 @@ var SearchResultItems = React.createClass({
   displayName: 'SearchResultItems',
   render: function() {
     return React.createElement('div', {}, this.props.items.map(function(item) {
-      return React.createElement(SearchResultItem, item, []);
+      var itemPlusKey = item;
+      // React needs this to keep track of lists.
+      itemPlusKey.key = item.id;
+      return React.createElement(SearchResultItem, itemPlusKey, []);
     }));
   }
 });
@@ -41,13 +40,39 @@ var SearchBox = React.createClass({
         ])
       ]),
       React.createElement('div', {}, [
-        React.createElement(SearchResultItems, {items: this.props.data.items}, [])
+        React.createElement(SearchResultItems, {items: this.props.items}, [])
       ])
     ]);
   }
 });
 
-React.render(React.createElement(SearchBox, {data: testData}), document.getElementById('main_container'));
+var TabMagic = React.createClass({
+  displayName: 'TabMagic',
+  getInitialState: function() {
+    return {
+      items: [],
+      loaded: false
+    };
+  },
+  componentDidMount: function() {
+    chrome.tabs.query({}, function(tabs) {
+      this.setState({
+        items: tabs,
+        loaded: true
+      });
+      // console.log(tabs);
+    }.bind(this));
+  },
+  render: function() {
+    if (this.props.loading) {
+      return React.createElement('div', {}, 'Loading...');
+    }
+    else {
+      return React.createElement(SearchBox, {items: this.state.items}, []);
+    }
+  }
+});
 
+React.render(React.createElement(TabMagic, {}, []), document.getElementById('main_container'));
 
 // console.log('\'Allo \'Allo! Popup');
