e802a606b9254db89fe85f4de05b62696856318a
Cai GoGwilt <gogwilt@gmail.com>
2014-12-09 13:49:36 -0800

    Very basic print out of all tabs when a tab is updated

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -7,3 +7,13 @@ chrome.runtime.onInstalled.addListener(function (details) {
 chrome.browserAction.setBadgeText({text: '\'Allo'});
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
+
+function updateTabList() {
+  chrome.tabs.query({}, function(tabs) {
+    console.log(tabs);
+  });
+}
+
+chrome.tabs.onCreated.addListener(updateTabList);
+chrome.tabs.onUpdated.addListener(updateTabList);
+chrome.tabs.onRemoved.addListener(updateTabList);
